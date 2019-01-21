011e6ae684913f0ea9fffcdaa9ef09707105e938
Igor Zaika <zaika.igor@pdffiller.team>
2019-01-21 17:22:27 +0200

    parsing payloads

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
@@ -25,15 +26,30 @@ const server = http.createServer((req, res) => {
   // Get the headers as an object
   const { headers } = req;
 
-  // Send the response
-  res.end('Hello world\n');
+  // Get the payload if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
 
-  // Log the request path
-  console.log(
-    `Request received on path: ${trimmedPath}, with method: ${method}, with params: ${JSON.stringify(
-      queryStringObject
-    )}, with headers: ${JSON.stringify(headers)}`
-  );
+  req.on('data', data => {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end('Hello world\n');
+
+    // Log the request path
+
+    // console.log(
+    //   `Request received on path: ${trimmedPath}, with method: ${method}, with params: ${JSON.stringify(
+    //     queryStringObject
+    //   )}, with headers: ${JSON.stringify(headers)}`
+    // );
+
+    console.log('Request received with this payload: ', buffer);
+  });
 });
 
 // Start the server, and have it listen on port 3000
